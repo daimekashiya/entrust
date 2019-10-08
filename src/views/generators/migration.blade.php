34f4a7be263c020efd79fdcaaa507773288c822c
Akashah Ahamad <akashah@2enapps.my>
2019-10-09 02:22:10 +0800

    migration error fix

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -14,60 +14,48 @@ class EntrustSetupTables extends Migration
     {
         DB::beginTransaction();
 
-        if (!Schema::hasTable('users')) {
-            
-        }
-
-
         // Create table for storing roles
         Schema::create('{{ $rolesTable }}', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
+            $table->increments('id');
             $table->string('name')->unique();
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
         });
-
-
-
-        // Create table for storing permissions
-        Schema::create('{{ $permissionsTable }}', function (Blueprint $table) {
-            $table->increments('id')->unsigned();
-            $table->string('name')->unique();
-            $table->string('display_name')->nullable();
-            $table->string('description')->nullable();
-            $table->timestamps();
-        });
-
-
-
 
         // Create table for associating roles to users (Many-to-Many)
         Schema::create('{{ $roleUserTable }}', function (Blueprint $table) {
-            $table->unsignedInteger('user_id');
-            $table->unsignedInteger('role_id');
-            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
 
             $table->foreign('user_id')->references('{{ $userKeyName }}')->on('{{ $usersTable }}')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('{{ $rolesTable }}')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            // $table->primary(['user_id', 'role_id']);
+            $table->primary(['user_id', 'role_id']);
+        });
+
+        // Create table for storing permissions
+        Schema::create('{{ $permissionsTable }}', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
         });
 
         // Create table for associating permissions to roles (Many-to-Many)
         Schema::create('{{ $permissionRoleTable }}', function (Blueprint $table) {
-            $table->unsignedInteger('permission_id');
-            $table->unsignedInteger('role_id');
-            $table->timestamps();
+            $table->integer('permission_id')->unsigned();
+            $table->integer('role_id')->unsigned();
 
             $table->foreign('permission_id')->references('id')->on('{{ $permissionsTable }}')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('{{ $rolesTable }}')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            // $table->primary(['permission_id', 'role_id']);
+            $table->primary(['permission_id', 'role_id']);
         });
 
         DB::commit();
